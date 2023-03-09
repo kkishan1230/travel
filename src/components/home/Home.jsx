dedea6ab79a84000d4b6895496f45b0ce90f84f6
kkishan1230 <kkishan1230@gmail.com>
2023-03-09 17:00:09 +0530

    initial commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../footer/Footer";
+import Main from "../main/Main";
+import Navbar from "../navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
